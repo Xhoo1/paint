d236a6bfcfdcef7739cb4171e5c8b8295453f14c
Xhoo1 <xhokabash@gmail.com>
2023-08-10 14:02:06 +0200

    adding js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-  createBoard(16);
+let color = "black";
 
-  
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(32);
+
+  /// add the event listener to get input only after user select it
+
+  let btnPopup = document.querySelector("#popup");
+
+  btnPopup.addEventListener("click", function () {
+    let size = selectSize();
+    createBoard(size);
+  });
 });
+
+// to create a board with grid using for loop
 
 function createBoard(size) {
   let board = document.querySelector(".board");
@@ -13,26 +24,50 @@ function createBoard(size) {
 
   for (let i = 0; i < numDivs; i++) {
     let box = document.createElement("div");
-    box.style.backgroundColor = "red";
+
+    box.addEventListener("mouseover", colorDiv);
 
     board.insertAdjacentElement("beforeend", box);
   }
 }
-function selectSize(){
 
-let input = prompt("Select size: ")
- let message = document.querySelector(".selectColor")
- if(input == ""){
+// to get the use input and pass it to the board
 
-   message.textContent = "Please enter a number";
+function selectSize() {
+  let input = prompt("Select size: ");
+  let message = document.querySelector(".selectColor");
+  if (input == "") {
+    message.textContent = "Please enter a number";
+  } else if (input < 0 || input > 100) {
+    message.textContent = "Please enter a number from 0 to 100";
+  } else {
+    message.textContent = "Feel free to paint";
+    return input;
+  }
+}
 
- } else if (input < 0 ||  input > 100){
+function colorDiv() {
+if( color == 'random'){
 
-  message.textContent = "Please enter a number from 0 to 100";
+this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
 
- } else{
-  message.textContent = "Feel free to paint";
+} else{
 
- }
+  this.style.backgroundColor = 'black';
+}
+
+}
+
+function setColor(colorChoice) {
+color = colorChoice;
+}
+
+// clear board by chaning color white
+
+function clearBoard(){
+
+let divs = document.querySelectorAll("div");
+divs.forEach((div) => div.style.backgroundColor = "white")
+
 
 }
